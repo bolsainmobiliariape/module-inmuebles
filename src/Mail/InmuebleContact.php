4aa47ddd1d167a5dbe64d4bf7e7a4da1ee74861c
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2022-03-11 12:58:24 -0600

    fix namespaces

--- src/Mail/InmuebleContact.php
+++ src/Mail/InmuebleContact.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Bolsainmobiliariape\ModuleInmuebleContact\Mail;
+namespace Bolsainmobiliariape\ModuleInmuebles\Mail;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
